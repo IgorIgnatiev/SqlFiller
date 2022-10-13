7d323a5530ff47053b70662fa26f3940aaed9803
IgorIgnatiev <04igorignatev@gmail.com>
2022-10-13 19:36:17 +0300

    fileRead tableName

--- MSSqlFiller/Program.cs
+++ MSSqlFiller/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
+using System.IO;
 namespace MSSqlFiller
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] Names = { "Vasia", "Petia", "Max", "Masha", "Igor", "Davud", "Aleksei" };
+            Console.WriteLine("Enter file path: ");
+            string path = Console.ReadLine();
+            string[] readText = File.ReadAllLines(path);
+            string tableName ="";
+            foreach (string str in readText)
+            {
+                if(str.Split(' ')[0] == "CREATE" && str.Split(' ')[1] == "TABLE")
+                {
+                    tableName = str.Split(' ')[2];
+                }
+            }
+            Console.WriteLine(tableName);
         }
     }
 }
